11683084c6dd2df4e4b207c69ce2174b10d7a641
Michiel Visser <opensource@webmichiel.nl>
2021-12-02 11:50:36 +0100

    Update run_all.sh to include double errors

--- simulation/run_all.sh
+++ simulation/run_all.sh
@@ -4,15 +4,17 @@ set -e
 CONTROLLERS=(BasicController WriteBackController)
 CODES=(IdentityCode ParityCode HammingCode ExtendedHammingCode HsiaoCode HsiaoConstructedCode DuttaToubaCode SheLiCode)
 
+RANGE=({0..24})
 LAMBDAS=(0.00100 0.00090 0.00080 0.00070 0.00060 0.00050 0.00040 0.00030 0.00020 0.00010 0.00005 0.00001)
+DOUBLE_RATES=(0.0 0.1)
 
 for controller in ${CONTROLLERS[@]}; do
     for code in ${CODES[@]}; do
         dir=${controller}-${code}
         cd ${dir}
         echo "Starting ${dir}"
-        ./simulation ../../firmware/coremark-perf.bin --seed 0 --lambda 0 --max-cycles 5100000 || true
-        parallel './simulation ../../firmware/coremark-perf.bin --seed {1} --lambda {2} --max-cycles 5100000' ::: {0..9} ::: ${LAMBDAS[@]} || true
+        nice parallel -j28 "./simulation ../../firmware/coremark-perf.bin --seed 0 --lambda 0 --double-rate {1} --max-cycles 5100000" ::: ${DOUBLE_RATES[@]} || true
+        nice parallel -j28 "./simulation ../../firmware/coremark-perf.bin --seed {1} --lambda {2} --double-rate {3} --max-cycles 5100000" ::: ${RANGE[@]} ::: ${LAMBDAS[@]} ::: ${DOUBLE_RATES[@]} || true
         echo "Done"
         cd ..
     done
